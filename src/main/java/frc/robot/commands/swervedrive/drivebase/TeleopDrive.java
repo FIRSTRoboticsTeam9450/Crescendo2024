76b713ee7f7e28dd95eb607b8ec979309e4ea2ff
LegendsNeverDl3 <1042154@apps.nsd.org>
2024-01-19 23:01:27 -0800

    update

--- src/main/java/frc/robot/commands/swervedrive/drivebase/TeleopDrive.java
+++ src/main/java/frc/robot/commands/swervedrive/drivebase/TeleopDrive.java
@@ -64,6 +64,7 @@ public class TeleopDrive extends Command
     swerve.drive(new Translation2d(xVelocity * swerve.maximumSpeed, yVelocity * swerve.maximumSpeed),
                  angVelocity * controller.config.maxAngularVelocity,
                  driveMode.getAsBoolean());
+    SmartDashboard.putNumber("MaxAngVel", controller.config.maxAngularVelocity);
   }
 
   // Called once the command ends or is interrupted.
