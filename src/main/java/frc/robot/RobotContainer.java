f87e44df820137bfd60c00646cf12d724b8c3d0e
KCheck25 <kcheck25@icloud.com>
2024-01-26 15:58:19 -0800

    pathplanner thing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -118,7 +118,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Path", true);
+    return drivebase.getAutonomousCommand("Straight Test", true);
   }
 
   public void setDriveMode()
