3ffc6b2e7ba9cc77a7a3a2a370f683b96a4777c8
Experance <100961120+Experance@users.noreply.github.com>
2024-03-03 17:30:37 -0800

    Glacier Peak day 2 changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -358,7 +358,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("RedThreeNote", true, true);
+    return drivebase.getAutonomousCommand("BlueTwoNote", true, true);
   }
 
   public void setDriveMode()
